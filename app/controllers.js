372148645be0a691619c9530a87437e0acaffd91
ukasz <pot.lukas@gmial.com>
2016-04-04 20:21:43 +0200

    kalendarz z bledami w konsoli

--- app/controllers.js
+++ app/controllers.js
@@ -1,117 +1,78 @@
+app.controller('KitchenSinkCtrl', function(moment, alert) {
 
-var app = angular.module("myApp")
+    var vm = this;
 
-    .controller('DatepickerDemoCtrl', function ($scope) {
-    $scope.today = function() {
-        $scope.dt = new Date();
-    };
-    $scope.today();
-
-    $scope.clear = function() {
-        $scope.dt = null;
-    };
-
-    $scope.inlineOptions = {
-        customClass: getDayClass,
-        minDate: new Date(),
-        showWeeks: true
-    };
-
-    $scope.dateOptions = {
-        dateDisabled: disabled,
-        formatYear: 'yy',
-        maxDate: new Date(2020, 5, 22),
-        minDate: new Date(),
-        startingDay: 1
-    };
-
-    // Disable weekend selection
-    function disabled(data) {
-        var date = data.date,
-          mode = data.mode;
-        return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
-    }
-
-    $scope.toggleMin = function() {
-        $scope.inlineOptions.minDate = $scope.inlineOptions.minDate ? null : new Date();
-        $scope.dateOptions.minDate = $scope.inlineOptions.minDate;
-    };
-
-    $scope.toggleMin();
-
-    $scope.open1 = function() {
-        $scope.popup1.opened = true;
-    };
-
-    $scope.open2 = function() {
-        $scope.popup2.opened = true;
-    };
-
-    $scope.setDate = function(year, month, day) {
-        $scope.dt = new Date(year, month, day);
-    };
-
-    $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
-    $scope.format = $scope.formats[0];
-    $scope.altInputFormats = ['M!/d!/yyyy'];
-
-    $scope.popup1 = {
-        opened: false
-    };
-
-    $scope.popup2 = {
-        opened: false
-    };
-
-    var tomorrow = new Date();
-    tomorrow.setDate(tomorrow.getDate() + 1);
-    var afterTomorrow = new Date();
-    afterTomorrow.setDate(tomorrow.getDate() + 1);
-    $scope.events = [
+    //These variables MUST be set as a minimum for the calendar to work
+    vm.calendarView = 'month';
+    vm.viewDate = new Date();
+    vm.events = [
         {
-            date: tomorrow,
-            status: 'full'
-        },
-        {
-            date: afterTomorrow,
-            status: 'partially'
+            title: 'An event',
+            type: 'warning',
+            startsAt: moment().startOf('week').subtract(2, 'days').add(8, 'hours').toDate(),
+            endsAt: moment().startOf('week').add(1, 'week').add(9, 'hours').toDate(),
+            draggable: true,
+            resizable: true
+        }, {
+            title: '<i class="glyphicon glyphicon-asterisk"></i> <span class="text-primary">Another event</span>, with a <i>html</i> title',
+            type: 'info',
+            startsAt: moment().subtract(1, 'day').toDate(),
+            endsAt: moment().add(5, 'days').toDate(),
+            draggable: true,
+            resizable: true
+        }, {
+            title: 'This is a really long event title that occurs on every year',
+            type: 'important',
+            startsAt: moment().startOf('day').add(7, 'hours').toDate(),
+            endsAt: moment().startOf('day').add(19, 'hours').toDate(),
+            recursOn: 'year',
+            draggable: true,
+            resizable: true
         }
     ];
 
-    function getDayClass(data) {
-        var date = data.date,
-          mode = data.mode;
-        if (mode === 'day') {
-            var dayToCheck = new Date(date).setHours(0,0,0,0);
+    vm.isCellOpen = true;
 
-            for (var i = 0; i < $scope.events.length; i++) {
-                var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
+    vm.eventClicked = function(event) {
+        alert.show('Clicked', event);
+    };
 
-                if (dayToCheck === currentDay) {
-                    return $scope.events[i].status;
-                }
-            }
-        }
+    vm.eventEdited = function(event) {
+        alert.show('Edited', event);
+    };
 
-        return '';
-    }
-})
-    .controller('BaseController', function ($scope) {
-      $scope.tabs = [
-          { title:'Ulubione',
-              content: [
-                  { name: 'ulubiony 1' },
-                  { name: 'ulubiony 2' },
-                  { name: 'ulubiony 3' }
-              ]
+    vm.eventDeleted = function(event) {
+        alert.show('Deleted', event);
+    };
 
-          },
-          { title:'Polecane', content: [
-              { name: 'polecane 1' },
-              { name: 'polecane 2' },
-              { name: 'polecane 3' }
-          ] }
-      ];
+    vm.eventTimesChanged = function(event) {
+        alert.show('Dropped or resized', event);
+    };
+
+    vm.toggle = function($event, field, event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+        event[field] = !event[field];
+    };
+
+});
+
+app.controller('BaseController', function ($scope) {
+    $scope.tabs = [
+        { title:'Ulubione',
+            content: [
+                { name: 'ulubiony 1' },
+                { name: 'ulubiony 2' },
+                { name: 'ulubiony 3' }
+            ]
+
+        },
+        { title:'Polecane', content: [
+            { name: 'polecane 1' },
+            { name: 'polecane 2' },
+            { name: 'polecane 3' }
+        ] }
+    ];
 
 
-  });
+});
