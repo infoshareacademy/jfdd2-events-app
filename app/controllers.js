2a214d9ace73e9abee4dd78f5a70f10accf4405b
karinabzowka <karinabzowka@hotmail.com>
2016-04-04 15:58:18 +0200

    Add favourite section

--- app/controllers.js
+++ app/controllers.js
@@ -95,4 +95,23 @@ var app = angular.module("myApp")
 
         return '';
     }
-});
+})
+    .controller('BaseController', function ($scope) {
+      $scope.tabs = [
+          { title:'Ulubione',
+              content: [
+                  { name: 'ulubiony 1' },
+                  { name: 'ulubiony 2' },
+                  { name: 'ulubiony 3' }
+              ]
+
+          },
+          { title:'Polecane', content: [
+              { name: 'polecane 1' },
+              { name: 'polecane 2' },
+              { name: 'polecane 3' }
+          ] }
+      ];
+
+
+  });
