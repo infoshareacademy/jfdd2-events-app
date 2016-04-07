973d11afd1fa8254069c72531993a20bc115bdb5
lpotas <pot.lukas@gmail.com>
2016-04-07 11:29:30 +0200

    rozdzielenie zmiany jezyka na oddzielny plik-kontroler

--- app/controllers.js
+++ app/controllers.js
@@ -41,7 +41,7 @@ app.controller('KitchenSinkCtrl', function(moment, alert, $log, eventsService, c
 
     function eventClicked(event) {
         var modalInstance = alert.show('Clicked', event);
-        
+        console.log(event);
         modalInstance.result.then(function (event) {
             console.log(event);
             vm.tabs[0].content.push(event);
@@ -70,33 +70,3 @@ app.controller('KitchenSinkCtrl', function(moment, alert, $log, eventsService, c
     }
 
 });
-
-app.controller('i18nCtrl', function($scope, $window, $ocLazyLoad, calendarConfig, moment) {
-
-    var vm = this;
-
-    vm.events = [];
-    vm.calendarView = 'month';
-    vm.viewDate = moment().startOf('month').toDate();
-
-    calendarConfig.dateFormatter = 'moment'; // use moment instead of angular for formatting dates
-    var originali18n = angular.copy(calendarConfig.i18nStrings);
-    calendarConfig.i18nStrings.weekNumber = 'Tydzień {week}';
-
-    $window.moment = $window.moment || moment;
-    $ocLazyLoad.load('https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/locale/pl.js').then(function() {
-        moment.locale('pl', {
-            week: {
-                dow: 1 // Monday is the first day of the week
-            }
-        });
-        moment.locale('pl'); // change the locale to french
-    });
-
-    $scope.$on('$destroy', function() {
-        moment.locale('en');
-        calendarConfig.i18nStrings = originali18n;
-    });
-
-});
-
