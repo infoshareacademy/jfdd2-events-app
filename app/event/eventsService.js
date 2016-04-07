edf03867594fcbad665229e8887261867ea2ae6e
BartlomiejMilewski <b.milewski762@gmail.com>
2016-04-07 16:11:28 +0200

    searchengine1.4

--- app/event/eventsService.js
+++ app/event/eventsService.js
@@ -6,28 +6,116 @@ app.service('eventsService', eventsService);
 function eventsService() {
   var events = [
     {
-      title: 'Pezet',
-      type: 'warning',
-      startsAt: moment().startOf('week').subtract(2, 'days').add(8, 'hours').toDate(),
-      endsAt: moment().startOf('week').add(1, 'week').add(9, 'hours').toDate(),
-      draggable: true,
-      resizable: true
+      title: 'Nazwa',
+      type: 'koncert',
+      startsAt: new Date("April 01, 2016 18:00:00"),
+      endsAt: new Date("April 01, 2016 22:00:00"),
     }, {
-      title: 'Rihanna',
-      type: 'info',
-      startsAt: moment().subtract(1, 'day').toDate(),
-      endsAt: moment().add(5, 'days').toDate(),
-      draggable: true,
-      resizable: true
+      title: 'Nazwa',
+      type: 'koncert',
+      startsAt: new Date("April 03, 2016 18:00:00"),
+      endsAt: new Date("April 03, 2016 22:00:00"),
     }, {
-      title: '50 Cent',
-      type: 'important',
-      startsAt: moment().startOf('day').add(7, 'hours').toDate(),
-      endsAt: moment().startOf('day').add(19, 'hours').toDate(),
-      recursOn: 'year',
-      draggable: true,
-      resizable: true
-    }
+      title: 'Nazwa',
+      type: 'koncert',
+      startsAt: new Date("April 05, 2016 18:00:00"),
+      endsAt: new Date("April 05, 2016 22:00:00"),
+    }, {
+      title: 'Nazwa',
+      type: 'teatr',
+      startsAt: new Date("April 01, 2016 18:00:00"),
+      endsAt: new Date("April 01, 2016 22:00:00"),
+    }, {
+      title: 'Nazwa',
+      type: 'teatr',
+      startsAt: new Date("April 08, 2016 18:00:00"),
+      endsAt: new Date("April 08, 2016 22:00:00"),
+    }, {
+      title: 'Nazwa',
+      type: 'kino',
+      startsAt: new Date("April 07, 2016 18:00:00"),
+      endsAt: new Date("April 07, 2016 22:00:00"),
+    }, {
+      title: 'Nazwa',
+      type: 'sport',
+      startsAt: new Date("April 10, 2016 18:00:00"),
+      endsAt: new Date("April 10, 2016 22:00:00"),
+    }, {
+      title: 'Nazwa',
+      type: 'sport',
+      startsAt: new Date("April 11, 2016 18:00:00"),
+      endsAt: new Date("April 11, 2016 22:00:00"),
+    }, {
+      title: 'Nazwa',
+      type: 'kino',
+      startsAt: new Date("April 08, 2016 18:00:00"),
+      endsAt: new Date("April 08, 2016 22:00:00"),
+    }, {
+      title: 'Nazwa',
+      type: 'sztuka',
+      startsAt: new Date("April 19, 2016 18:00:00"),
+      endsAt: new Date("April 19, 2016 22:00:00"),
+    }, {
+      title: 'Nazwa',
+      type: 'rozrywka',
+      startsAt: new Date("April 19, 2016 18:00:00"),
+      endsAt: new Date("April 19, 2016 22:00:00"),
+    }, {
+      title: 'Nazwa',
+      type: 'kino',
+      startsAt: new Date("April 25, 2016 18:00:00"),
+      endsAt: new Date("April 25, 2016 22:00:00"),
+    }, {
+      title: 'Nazwa',
+      type: 'sztuka',
+      startsAt: new Date("April 14, 2016 18:00:00"),
+      endsAt: new Date("April 14, 2016 22:00:00"),
+    }, {
+      title: 'Nazwa',
+      type: 'teatr',
+      startsAt: new Date("April 17, 2016 18:00:00"),
+      endsAt: new Date("April 17, 2016 22:00:00"),
+    }, {
+      title: 'Nazwa',
+      type: 'rozrywka',
+      startsAt: new Date("April 14, 2016 18:00:00"),
+      endsAt: new Date("April 14, 2016 22:00:00"),
+    }, {
+      title: 'Nazwa',
+      type: 'sport',
+      startsAt: new Date("April 20, 2016 18:00:00"),
+      endsAt: new Date("April 20, 2016 22:00:00"),
+    }, {
+      title: 'Nazwa',
+      type: 'sztuka',
+      startsAt: new Date("April 15, 2016 18:00:00"),
+      endsAt: new Date("April 15, 2016 22:00:00"),
+    }, {
+      title: 'Nazwa',
+      type: 'sport',
+      startsAt: new Date("April 14, 2016 18:00:00"),
+      endsAt: new Date("April 14, 2016 22:00:00"),
+    }, {
+      title: 'Nazwa',
+      type: 'kino',
+      startsAt: new Date("April 14, 2016 18:00:00"),
+      endsAt: new Date("April 14, 2016 22:00:00"),
+    }, {
+      title: 'Nazwa',
+      type: 'koncert',
+      startsAt: new Date("April 29, 2016 18:00:00"),
+      endsAt: new Date("April 29, 2016 22:00:00"),
+    }, {
+      title: 'Nazwa',
+      type: 'sport',
+      startsAt: new Date("April 28, 2016 18:00:00"),
+      endsAt: new Date("April 28, 2016 22:00:00"),
+    }, {
+      title: 'Nazwa',
+      type: 'sztuka',
+      startsAt: new Date("April 06, 2016 18:00:00"),
+      endsAt: new Date("April 06, 2016 22:00:00"),
+    },
   ];
 
   this.getEvents = function() {
