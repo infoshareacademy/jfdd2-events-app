aaefa9836a018621a8b316a28af55675c21aa088
BartlomiejMilewski <b.milewski762@gmail.com>
2016-04-11 12:55:22 +0200

    login v1.0

--- app/socialMediaLogin/login.js
+++ app/socialMediaLogin/login.js
@@ -1,29 +1,21 @@
 /**
  * Created by ScipioAfricanus on 2016-04-10.
  */
+function onSuccess(googleUser) {
+    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+    $('loginBackground').hide();
+}
+
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail());
-};
-
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        console.log('User signed out.');
-    });
-};
-
-function onSuccess(googleUser) {
-    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
 }
-function onFailure(error) {
-    console.log(error);
-}
+
 function renderButton() {
-    gapi.signin2.render('my-signin2', {
+    gapi.signin2.render('g-signin2', {
         'scope': 'profile email',
         'width': 240,
         'height': 50,
@@ -32,4 +24,16 @@ function renderButton() {
         'onsuccess': onSuccess,
         'onfailure': onFailure
     });
+}
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+}
+
+
+function onFailure(error) {
+    console.log(error);
 }
